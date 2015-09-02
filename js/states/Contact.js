5bf80f21efcbdf91f61a07cd9341803eb2baab16
Mark Duiker <mark.duiker@kidshelpphone.ca>
2015-09-02 10:21:14 -0400

    Expanded event tracking

--- js/states/Contact.js
+++ js/states/Contact.js
@@ -36,6 +36,7 @@ Climb.Contact.prototype = {
 		var MessageBt = this.game.add.sprite(this.game.width / 2, messageButtonH, "square");
 		createBt(MessageBt, text, false, false, "icon-chat");
 		MessageBt.events.onInputUp.add(function() {
+			trackEvent("Message a Counsellor clicked", Vent.game.state.getCurrentState().key+" screen");
 			message_brotalk();
 		}, this);
 
@@ -44,14 +45,16 @@ Climb.Contact.prototype = {
 		var PhoneBt = this.game.add.sprite(this.game.width / 2, phoneButtonH, "square");
 		createBt(PhoneBt, text, false, false, "icon-phone");
 		PhoneBt.events.onInputUp.add(function() {
+			trackEvent("Phone a Counsellor clicked", Vent.game.state.getCurrentState().key+" screen");
 			phone_brotalk();
 		}, this);
 
 		// More info
 		text = counsellor_txt[0];
-		var PhoneBt = this.game.add.sprite(this.game.width / 2, this.game.height / 2 + 90, "square");
-		createBt(PhoneBt, text, false);
-		PhoneBt.events.onInputUp.add(function() {
+		var LearnBt = this.game.add.sprite(this.game.width / 2, this.game.height / 2 + 90, "square");
+		createBt(LearnBt, text, false);
+		LearnBt.events.onInputUp.add(function() {
+			trackEvent("Learn About Counsellors clicked", Vent.game.state.getCurrentState().key+" screen");
 			moreAbout();
 		}, this);
 
@@ -66,6 +69,7 @@ Climb.Contact.prototype = {
 function phone_brotalk() {
 	var r = confirm("Are you sure you want to dial Kids Help Phone's number?");
 	if (r === true) {
+		trackEvent("Phone a Counsellor confirmed", Vent.game.state.getCurrentState().key+" screen");
 		window.location = phone_url;
 	} else {
 		// do nothing if cancel is pressed
@@ -75,6 +79,7 @@ function phone_brotalk() {
 function message_brotalk() {
 	var r = confirm("Are you sure you want to leave this page?");
 	if (r === true) {
+		trackEvent("Message a Counsellor confirmed", Vent.game.state.getCurrentState().key+" screen");
 		openInNewTab(chat_url);
 	} else {
 		// do nothing if cancel is pressed
@@ -84,6 +89,7 @@ function message_brotalk() {
 function moreAbout() {
 	var r = confirm("Are you sure you want to leave this page?");
 	if (r === true) {
+		trackEvent("Learn About Counsellors confirmed", Vent.game.state.getCurrentState().key+" screen");
 		openInNewTab(counsellor_url);
 	} else {
 		// do nothing if cancel is pressed
